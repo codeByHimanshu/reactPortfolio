8e5facf75ddd87d30a8c68d13ef34446f387d7e4
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-02-28 17:17:05 +0530

    portfolio created

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import About from "./components/About"
+function App() {
+
+
+  return (
+    <>
+<About />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/About.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+    
+return(
+    <>
+    <div className="p-4 m-8 bg-gray-900 animate__animated animate__fadeInUp animate__delay-2s">
+
+    hii himanshu is here
+    </div>
+    </>
+)
+
+}
